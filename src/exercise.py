1911eca52f464d9e88191fd43962e9e8b31ef23b
Jaime Santos <jaimepereirasantos123@gmail.com>
2020-10-21 19:24:50 +0000

    ex

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,12 @@
 def main():
     #write your code below this line
+    print_from_number_to_one(4)
+
+
+def print_from_number_to_one(number):
+    for i in range(number, 0):
+        print(i)
+
 
 if __name__ == '__main__':
     main()
